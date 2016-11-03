2eaf8e314c320d4aa42b1266355d97980f0c669e
Austin Lanari <austin.lanari@gmail.com>
2016-11-02 23:24:39 -0400

    Begin to reformat auth

--- app/controllers/mails_controller.rb
+++ app/controllers/mails_controller.rb
@@ -2,6 +2,7 @@ class MailsController < ApplicationController
   # for setup
   def new
     access_token = session[:tokens]["access_token"]
+    # TODO: redirect user here if tokens are invalid, don't pass auth to clientside
     the_headers = {
       "Authorization" => "Bearer #{access_token}",
       "Host" => "crest-tq.eveonline.com",
