3e30981e0a3b403eb56aeeccd687f1a5fa24d3b3
Austin Lanari <austin.lanari@gmail.com>
2016-11-20 10:25:23 -0500

    Add tabs that switch active classes on click

--- app/assets/javascripts/components/App.es6.jsx
+++ app/assets/javascripts/components/App.es6.jsx
@@ -5,10 +5,15 @@ class App extends React.Component {
       contacts: [],
       recipients: [],
       remove: "",
-      submitted: false
+      submitted: false,
+      contactTabs: {"All Contacts": "active",
+                    "Friendly": "",
+                    "Corp": ""}
     };
-    this.changeRecip = this.changeRecip.bind(this)
-    this.handleContacts = this.handleContacts.bind(this)
+    this.changeRecip = this.changeRecip.bind(this);
+    this.handleContacts = this.handleContacts.bind(this);
+    this.filterContacts = this.filterContacts.bind(this);
+    this.updateTab = this.updateTab.bind(this);
   }
 
   changeRecip(recipient) {
@@ -34,6 +39,20 @@ class App extends React.Component {
     this.setState({recipients: []}, this.setState({submitted: bool}))
   }
 
+  filterContacts(newTab) {
+    this.updateTab(newTab);
+
+  }
+
+  updateTab(newTab) {
+    tabDup = this.state.contactTabs;
+    for (var key in tabDup) {
+      tabDup[key] = ""; 
+    }
+    tabDup[newTab] = "active"
+    this.setState({contactTabs: tabDup})
+  }
+
   componentDidMount() {
     if (this.props.isAuthorized) {
       localStorage.access_token = this.props.tokens.access_token;
@@ -46,7 +65,7 @@ class App extends React.Component {
     return(
       <div className="row">
         <MailForm data={this.state.recipients} submitDone={this.handleContacts}/>
-        <ContactList data={this.state.contacts} updateTo={this.changeRecip} submitStatus={this.state.submitted} />
+        <ContactList data={this.state.contacts} updateTo={this.changeRecip} submitStatus={this.state.submitted} filter={this.filterContacts} tabs={this.state.contactTabs} />
       </div>
       )
   }
